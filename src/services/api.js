f2d82b86d1ad70e8ae0b707921dc8c5660eb1198
Oshin Lilani <oshinlilani08@gmail.com>
2026-03-06 16:28:28 +0530

    Updated API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://gidy-assignment-backend.onrender.com"
 });
 
 export const getProfile = () => API.get("/profile");
